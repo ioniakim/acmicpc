05a6870816e11fc01704fe82fe4c1cb5b73392d2
S W Kim <ionia.kim@gmail.com>
2024-03-16 22:29:40 +0900

    Add 2884 to stage 2

---
+++ stage002/2884.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)?;
+
+    let nums: Vec<i8> = input.split_whitespace()
+        .map(str::parse)
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let hour: i8 = nums[0];
+    let minute: i8 = nums[1];
+
+    let mut adjusted_hour = hour;
+    let mut adjusted_minute = minute;
+
+    if minute < 45 {
+        adjusted_hour = adjusted_hour - 1;
+        adjusted_minute = 60 - 45 + adjusted_minute;
+    } else {
+        adjusted_minute -= 45;
+    }
+
+    if adjusted_hour < 0 {
+        adjusted_hour = 23;
+    }
+
+    println!("{adjusted_hour} {adjusted_minute}");
+
+    Ok(())
+}
