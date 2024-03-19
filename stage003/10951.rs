0fd838f2a9e873ef517bbb9959f30b2561409882
S W Kim <ionia.kim@gmail.com>
2024-03-19 22:29:28 +0900

    Use lines() method instead of read_line() in 10951

--- stage003/10951.rs
+++ stage003/10951.rs
@@ -4,20 +4,12 @@ use io::Write;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut out = io::BufWriter::new(io::stdout());
 
-    let mut input = String::new();
-    loop {
-        match io::stdin().read_line(&mut input) {
-            Ok(0) => break,
-            Ok(_) => {
-                let sum = input.split_whitespace()
-                    .map(str::parse::<u8>)
-                    .sum::<Result<u8, _>>()?;
-                input.clear();
-
-                writeln!(out, "{}", sum)?;
-            },
-            Err(_) => break,
-        }
+    for line in io::stdin().lines() {
+        let line = line?;
+        let sum = line.split_whitespace()
+            .map(str::parse::<u8>)
+            .sum::<Result<u8, _>>()?;
+        writeln!(out, "{}", sum)?;
     }
     Ok(())
 }
