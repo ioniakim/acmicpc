fd5b0becd4d9cd84d61343bbc5fc87265b0839e0
S W Kim <ionia.kim@gmail.com>
2024-03-19 23:40:28 +0900

    Add 10871 to stage 4 in functional way

---
+++ stage004/10871.rs
@@ -0,0 +1,32 @@
+use std::io;
+use io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut out = io::BufWriter::new(io::stdout());
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    let info: Vec<i32> = input.split_whitespace()
+        .map(str::parse)
+        .collect::<Result<Vec<_>, _>>()?;
+
+    if info.len() != 2 {
+        return Err("Invalid format".into());
+    }
+
+    let x = info[1];
+
+    input.clear();
+    io::stdin().read_line(&mut input)?;
+    let result = input.split_whitespace()
+        .map(str::parse::<i32>)
+        .map(Result::unwrap)
+        .filter(|&r| r < x)
+        .map(|v| v.to_string())
+        .collect::<Vec<_>>()
+        .join(" ");
+
+    writeln!(out, "{}", result)?;
+
+    Ok(())
+}
