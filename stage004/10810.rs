f360e7033a5dc35bd3fee3f231f3615ab8267f23
S W Kim <ionia.kim@gmail.com>
2024-03-20 22:33:46 +0900

    Add 10810 to stage 4

---
+++ stage004/10810.rs
@@ -0,0 +1,32 @@
+use std::io;
+use io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut out = io::BufWriter::new(io::stdout());
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    let first_values: Vec<u8> = input.split_whitespace()
+        .map(str::parse)
+        .collect::<Result<Vec<_>, _>>()?;
+    let n = first_values[0];
+    let m = first_values[1];
+
+    let mut baskets = vec![0u8; n as usize];
+
+    for _ in 0..m {
+        input.clear();
+        io::stdin().read_line(&mut input)?;
+        let method: Vec<u8> = input.split_whitespace()
+            .map(str::parse)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        for i in (method[0] - 1)..=(method[1] - 1) {
+            baskets[i as usize] = method[2];
+        }
+    }
+
+    writeln!(out, "{}", baskets.iter().map(u8::to_string).collect::<Vec<_>>().join(" "))?;
+
+    Ok(())
+}
