4a9f8c11ead1a6cabe0c8d3e912a049dcbb770d5
S W Kim <ionia.kim@gmail.com>
2024-03-24 00:51:12 +0900

    Reverse number in 2908

--- stage005/2908.rs
+++ stage005/2908.rs
@@ -4,14 +4,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
 
-    let nums: Vec<i32> = input.split_whitespace()
-        .map(|s| {
-            s.chars().rev().collect::<String>()
-        })
-        .map(|s| s.parse::<i32>())
+    let nums: Vec<usize> = input.split_whitespace()
+        .map(|s| s.parse::<usize>())
         .collect::<Result<Vec<_>, _>>()?;
 
-    println!("{}", std::cmp::max(nums[0], nums[1]));
+    println!("{}", std::cmp::max(rev_num(nums[0]), rev_num(nums[1])));
 
     Ok(())
+}
+
+#[allow(dead_code)]
+fn rev_num(mut num: usize) -> usize {
+    let mut rev = 0usize;
+    while num > 0 {
+        rev *= 10;
+        rev += num % 10;
+        num /= 10;
+    }
+
+    rev
 }
