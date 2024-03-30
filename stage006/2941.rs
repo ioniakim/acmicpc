ee7ced65ae9a32505987f2169be690b70e3420be
S W Kim <ionia.kim@gmail.com>
2024-03-31 00:45:46 +0900

    Fix out of index in 2941

--- stage006/2941.rs
+++ stage006/2941.rs
@@ -5,21 +5,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
 
-    let word = input.trim().as_bytes();
+    let word_bytes = input.trim().as_bytes();
 
     let mut count = 0;
 
     let mut pos = 0;
-    while pos < word.len() {
-        pos += match word[pos] as char {
-            'c' => c_start(&word, pos + 1),
-            'd' => d_start(&word, pos + 1),
-            'l' => l_start(&word, pos + 1),
-            'n' => n_start(&word, pos + 1),
-            's' => s_start(&word, pos + 1),
-            'z' => z_start(&word, pos + 1),
-            _ => 0,
-        };
+    while pos < word_bytes.len() {
+        if pos + 1 < word_bytes.len() {
+            pos += match word_bytes[pos] as char {
+                'c' => c_start(&word_bytes, pos + 1),
+                'd' => d_start(&word_bytes, pos + 1),
+                'l' => l_start(&word_bytes, pos + 1),
+                'n' => n_start(&word_bytes, pos + 1),
+                's' => s_start(&word_bytes, pos + 1),
+                'z' => z_start(&word_bytes, pos + 1),
+                _ => 0,
+            };
+        }
 
         count += 1;
         pos += 1;
@@ -29,46 +31,46 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn c_start(word: &[u8], pos: usize) -> usize {
-    match word[pos] as char {
+fn c_start(word_bytes: &[u8], pos: usize) -> usize {
+    match word_bytes[pos] as char {
         '=' | '-'  => 1,
         _ => 0,
     }
 }
 
-fn d_start(word: &[u8], pos: usize) -> usize {
-    match word[pos] as char {
+fn d_start(word_bytes: &[u8], pos: usize) -> usize {
+    match word_bytes[pos] as char {
         '-' => 1,
         'z' => {
-            if word[pos + 1] as char == '=' { return 2 } else { 0 }
+            if word_bytes.len() > pos + 1 && word_bytes[pos + 1] as char == '=' { return 2 } else { 0 }
         }
         _ => 0,
     }
 }
 
-fn l_start(word: &[u8], pos: usize) -> usize {
-    match word[pos] as char {
+fn l_start(word_bytes: &[u8], pos: usize) -> usize {
+    match word_bytes[pos] as char {
         'j' => 1,
         _ => 0,
     }
 }
 
-fn n_start(word: &[u8], pos: usize) -> usize {
-    match word[pos] as char {
+fn n_start(word_bytes: &[u8], pos: usize) -> usize {
+    match word_bytes[pos] as char {
         'j' => 1,
         _ => 0,
     }
 }
 
-fn s_start(word: &[u8], pos: usize) -> usize {
-    match word[pos] as char {
+fn s_start(word_bytes: &[u8], pos: usize) -> usize {
+    match word_bytes[pos] as char {
         '=' => 1,
         _ => 0,
     }
 }
 
-fn z_start(word: &[u8], pos: usize) -> usize {
-    match word[pos] as char {
+fn z_start(word_bytes: &[u8], pos: usize) -> usize {
+    match word_bytes[pos] as char {
         '=' => 1,
         _ => 0,
     }
