0e747301f86c7d3c962072b82c93a38e24b311a8
S W Kim <ionia.kim@gmail.com>
2024-04-01 23:00:51 +0900

    Change printing result into functional way in 10798

--- stage007/10798.rs
+++ stage007/10798.rs
@@ -1,8 +1,6 @@
 use std::io;
-use io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut out = io::BufWriter::new(io::stdout());
     let mut max_len = 0usize;
     const LINE: usize = 5;
     let mut board = [[0 as char; 15]; LINE];
@@ -17,14 +15,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             });
     }
 
-    for j in 0..max_len {
-        for i in 0..5 {
-            if board[i][j] != 0 as char {
-                write!(out, "{}", board[i][j])?;
-            }
-        }
-    }
-    writeln!(out, "")?;
+    (0..max_len).for_each(|j| {
+        (0..5).map(|i| board[i][j])
+            .filter(|&c| c != 0 as char)
+            .for_each(|c| print!("{c}"))
+    });
 
     Ok(())
 }
