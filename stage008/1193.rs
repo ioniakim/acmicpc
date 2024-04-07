f84772117d24d1862cbdaff773e1deedaacfd01f
S W Kim <ionia.kim@gmail.com>
2024-04-07 12:57:31 +0900

    Calcuate the numerator and denominator with math in 1193

--- stage008/1193.rs
+++ stage008/1193.rs
@@ -1,6 +1,13 @@
 /// faction
 /// 1/1, 1/2, 2/1, 3/1, 2/2, 1/3, 1/4, 2/3, 3/2, 4/1, ...
 ///
+/// 1, 2, 3, 4, 5
+/// even: numerator in ascending order
+/// odd: denomiator in ascending order
+///
+/// 1, 1 + 2, 3 + 3, 6 + 4, 10 + 5
+///
+///
 /// seed = 1..
 /// numerator = 1..=seed
 /// denominator = (1..=seed).rev()
@@ -14,33 +21,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let x = input.trim().parse::<u32>()?;
 
-    let mut seed = 1;
-    let mut count = 0;
-
-    'outer: loop {
-
-        match seed % 2 {
-            0 => {
-                for (numerator, denominator) in (1..=seed).zip((1..=seed).rev()) {
-                    count += 1;
-                    if count == x {
-                        println!("{numerator}/{denominator}");
-                        break 'outer;
-                    }
-                }
-            },
-            _ => {
-                for (numerator, denominator) in ((1..=seed).rev()).zip(1..=seed) {
-                    count += 1;
-                    if count == x {
-                        println!("{numerator}/{denominator}");
-                        break 'outer;
-                    }
-                }
-            },
-        }
-        seed += 1;
+    // Locate the turn and the max value
+    let mut turn_max = 1;
+    let mut turn = 1;
+    while turn_max < x {
+        turn += 1;
+        turn_max += turn;
     }
 
+    // Find numerator and denomiator according to the turn.
+    match turn % 2 {
+        0 => {
+            let denominator = turn_max - x + 1;
+            let numerator = turn - denominator + 1;
+            println!("{numerator}/{denominator}");
+        },
+        _ => {
+            let numerator = turn_max - x + 1;
+            let denominator = turn - numerator + 1;
+            println!("{numerator}/{denominator}");
+        }
+    }
     Ok(())
 }
+
+
