9947ccf39da87f6384960163945aabbd3396d0a5
S W Kim <ionia.kim@gmail.com>
2024-04-14 21:47:04 +0900

    Use div_ceil method in 2869

--- stage008/2869.rs
+++ stage008/2869.rs
@@ -1,20 +1,18 @@
+/// n*A - B*(n - 1) >= V
+/// n*A - n*B + B >= V
+/// n*(A - B) >= V - B
+/// n >= (V - B) / (A - B)
 use std::io;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     let mut iter = input.split_whitespace();
-    let climb = iter.next().unwrap_or("No Climb Argument").parse::<u32>()?;
-    let slip = iter.next().unwrap_or("No Slip Argument").parse::<u32>()?;
-    let tree = iter.next().unwrap_or("No Tree Argument").parse::<u32>()?;
+    let climb = iter.next().unwrap_or("").parse::<u32>()?;
+    let slip = iter.next().unwrap_or("").parse::<u32>()?;
+    let tree = iter.next().unwrap_or("").parse::<u32>()?;
 
-    let mut height = climb;
-    let mut days = 1;
-    while height < tree {
-        height -= slip;
-        height += climb;
-        days += 1;
-    }
+    let days = (tree - slip).div_ceil(climb - slip);
 
     println!("{days}");
 
