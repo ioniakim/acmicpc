c60e90a5d98e5ff4ccf66ac838e3c225ceda5db9
S W Kim <ionia.kim@gmail.com>
2024-04-14 20:04:49 +0900

    Make unwrap return error str in 2869

--- stage008/2869.rs
+++ stage008/2869.rs
@@ -4,9 +4,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     let mut iter = input.split_whitespace();
-    let climb = iter.next().unwrap().parse::<u32>()?;
-    let slip = iter.next().unwrap().parse::<u32>()?;
-    let tree = iter.next().unwrap().parse::<u32>()?;
+    let climb = iter.next().unwrap_or("No Climb Argument").parse::<u32>()?;
+    let slip = iter.next().unwrap_or("No Slip Argument").parse::<u32>()?;
+    let tree = iter.next().unwrap_or("No Tree Argument").parse::<u32>()?;
 
     let mut height = climb;
     let mut days = 1;
@@ -17,5 +17,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     println!("{days}");
+
     Ok(())
 }
