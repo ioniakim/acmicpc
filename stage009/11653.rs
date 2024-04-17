9e022c416daedd9867be304da2447f65e7ef95e9
S W Kim <ionia.kim@gmail.com>
2024-04-17 22:27:54 +0900

    Add iterator example to 11653

--- stage009/11653.rs
+++ stage009/11653.rs
@@ -4,10 +4,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     let n = input.trim().parse::<usize>()?;
-    factorize_while(n);
+    factorize_iterator_adapter(n);
     Ok(())
 }
 
+#[allow(dead_code)]
 fn factorize_while(number: usize) {
     let mut number = number;
     let mut divisor = 2;
@@ -22,4 +23,32 @@ fn factorize_while(number: usize) {
     if number != 1 {
         println!("{number}");
     }
+}
+
+/// It doesn't work correctly. Only reference it as an example of an iterator usage
+#[allow(dead_code)]
+fn factorize_iterator(number: usize) {
+    // Generate potential divisors up to the square root of the number
+    let divisors = (2..=(number as f64).sqrt() as usize)
+        .filter(|divisor| number % divisor == 0) // It can't allow repetitive factors. eg) if number is 4, 2 and 2 must be printed but only one 2.
+        .inspect(|divisor| println!("{divisor}"));
+
+    // Handle a potential remaing prime factor
+    if let Some(last_factor) = divisors.last() {
+        if last_factor * last_factor < number {
+            println!("{}", number / last_factor);
+        }
+    }
+}
+
+/// It doesn't work correctly. Only reference it as an example of an iterator usage
+///
+#[allow(dead_code)]
+fn factorize_iterator_adapter(number: usize) {
+    (2..=(number as f64).sqrt() as usize)
+        .take_while(|&i| i * i <= number)
+        .filter(|&i| number % i == 0)
+        .for_each(|i| {
+            println!("{i}");
+        });
 }
