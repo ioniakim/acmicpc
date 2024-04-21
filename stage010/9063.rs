fcebd501024a27d1eca6d4c4d1bdfd4381736ef3
S W Kim <ionia.kim@gmail.com>
2024-04-21 18:18:48 +0900

    Replace vec with min/max values in 9063

--- stage010/9063.rs
+++ stage010/9063.rs
@@ -1,20 +1,27 @@
 use std::error::Error;
 use std::io;
+use std::cmp::{max, min};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
     let n = input.trim().parse::<usize>()?;
 
-    let mut x_axis: Vec<isize> = vec![];
-    let mut y_axis: Vec<isize> = vec![];
+    let mut x_min = isize::MAX;
+    let mut x_max = isize::MIN;
+    let mut y_min = isize::MAX;
+    let mut y_max = isize::MIN;
 
     for line in io::stdin().lines().take(n) {
         let line = line?;
         let mut axis = line.split_whitespace().flat_map(str::parse::<isize>);
-        x_axis.push(axis.next().ok_or("Invalid".to_owned())?);
-        y_axis.push(axis.next().ok_or("Invalid".to_owned())?);
+        let x = axis.next().ok_or("Invalid".to_owned())?;
+        x_min = min(x_min, x);
+        x_max = max(x_max, x);
 
+        let y = axis.next().ok_or("Invalid".to_owned())?;
+        y_min = min(y_min, y);
+        y_max = max(y_max, y);
     }
 
     if n == 1 {
@@ -22,8 +29,8 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Ok(());
     }
 
-    let width =  (x_axis.iter().max().ok_or("None".to_owned())? - x_axis.iter().min().ok_or("None".to_owned())?).abs();
-    let height = (y_axis.iter().max().ok_or("None".to_owned())? - y_axis.iter().min().ok_or("None".to_owned())?).abs();
+    let width =  (x_max - x_min).abs();
+    let height = (y_max - y_min).abs();
 
     println!("{}", width * height);
     Ok(())
