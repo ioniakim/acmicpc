dda265ba664067eaa9f652c9a9586dd2a0d961cc
S W Kim <ionia.kim@gmail.com>
2024-04-28 00:54:03 +0900

    Add concise solution to 2839 in stage 12

--- stage012/2839.rs
+++ stage012/2839.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     io::stdin().read_line(&mut input)?;
     let n: u16 = input.trim().parse()?;
 
-    println!("{}", count_packages(n));
+    println!("{} {}", count_packages(n), count_packages2(n));
     Ok(())
 }
 
@@ -28,6 +28,23 @@ fn count_packages(n: u16) -> i16 {
 
     if n % THREE == 0 {
         return (n / THREE) as i16;
+    }
+
+    -1
+}
+
+fn count_packages2(n: u16) -> i16 {
+    const FIVE: u16 = 5;
+    const THREE: u16 = 3;
+    let three_max = n / THREE;
+    for three_count in 0..three_max {
+        let remain = n - three_count * THREE;
+        if remain % FIVE == 0 {
+            return (remain / FIVE + three_count) as i16;
+        }
+    }
+    if n % THREE == 0 {
+        return three_max as i16;
     }
 
     -1
