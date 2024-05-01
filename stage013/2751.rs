1b2e279cf1f433b703c8e58349fe84707b377d21
S W Kim <ionia.kim@gmail.com>
2024-05-01 19:58:32 +0900

    Change way to read stdin to improve performance in 2751

--- stage013/2751.rs
+++ stage013/2751.rs
@@ -2,16 +2,24 @@ use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut out = std::io::BufWriter::new(std::io::stdout());
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input)?;
-    let n = input.trim().parse()?;
+    // let mut input = String::new();
+    // std::io::stdin().read_line(&mut input)?;
+    // let n = input.trim().parse()?;
 
-    let mut data = std::io::stdin().lines().take(n)
-        .filter_map(Result::ok)
-        .map(|line| line.parse::<i32>())
-        .collect::<Result<Vec<_>, _>>()?;
+    // let mut data = std::io::stdin().lines().take(n)
+    //     .filter_map(Result::ok)
+    //     .map(|line| line.parse::<i32>())
+    //     .collect::<Result<Vec<_>, _>>()?;
 
-    quick_sort(&mut data);
+    let input = std::io::read_to_string(std::io::stdin())?;
+    let mut numbers: Vec<i32> = input.trim().split_ascii_whitespace()
+        .flat_map(str::parse)
+        .collect();
+
+    let data = &mut numbers[1..];
+
+    // quick_sort(&mut data);
+    data.sort();
 
     for e in data {
         writeln!(out, "{}", e)?;
