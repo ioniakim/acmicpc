8ec82a2092785cf37eba61cf128781fcccd9ab38
S W Kim <ionia.kim@gmail.com>
2024-05-03 22:27:30 +0900

    Add 10989 to stage 13. Counting sort

---
+++ stage013/10989.rs
@@ -0,0 +1,22 @@
+use std::io;
+use io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut out = io::BufWriter::new(io::stdout());
+    let mut count = [0usize; 10_001];
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input)?;
+    let n = input.trim().parse::<usize>()?;
+    for line in io::stdin().lines().take(n) {
+        let line = line?;
+        count[line.parse::<usize>()?] += 1;
+    }
+
+    for (v, &c) in count.iter().enumerate() {
+        for _ in 0..c {
+            writeln!(out, "{v}")?;
+        }
+    }
+    Ok(())
+}
