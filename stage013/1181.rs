1837b27b41961d580bd2c9e0f4a59612291d5cd9
S W Kim <ionia.kim@gmail.com>
2024-05-05 22:25:20 +0900

    Add 1181 to stage 13 by implmenting sort but fail the test

---
+++ stage013/1181.rs
@@ -0,0 +1,125 @@
+/// [ ] &str and &String are not compatible in function pointer
+/// [ ] ways to return a closure. eg) using impl, Box<dyn>. why not specifying in where clause
+use std::cmp::Ordering;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input)?;
+    let n = input.trim().parse::<usize>()?;
+
+    let mut strings: Vec<String> = std::io::stdin().lines().take(n)
+        .flat_map(|s| s)
+        .collect();
+
+    shuffle_qsort_by(&mut strings, length_lexical_order);
+
+    strings.dedup();
+
+    let mut out = std::io::BufWriter::new(std::io::stdout());
+    for s in strings {
+        writeln!(out, "{s}")?;
+    }
+    Ok(())
+}
+
+
+/// Have to find to use &str instead of &&str
+fn length_lexical_order(s1: &String, s2: &String) -> std::cmp::Ordering {
+    // length order
+    if s1.len() < s2.len() {
+        return Ordering::Less;
+    } else if s1.len() > s2.len() {
+        return Ordering::Greater;
+    }
+
+    // lexical order
+    let mut two_chars = s1.bytes().zip(s2.bytes());
+
+    while let Some((c1, c2)) = two_chars.next() {
+        if c1 < c2 {
+            return Ordering::Less;
+        } else if c1 > c2 {
+            return Ordering::Greater;
+        }
+    }
+
+    Ordering::Equal
+}
+
+fn shuffle_qsort_by<T, F>(elements: &mut [T], cmp: F)
+where F: Fn(&T, &T) -> std::cmp::Ordering {
+    shuffle(elements);
+
+    qsort_by(elements, &cmp);
+}
+
+fn gen_rand(seed: usize) -> impl FnMut() -> usize {
+    const MULTIPLY: usize = 1664525;
+    const INCREMENT: usize = 1013904223;
+    let mut rand = seed;
+    return move || {
+        rand = rand.wrapping_mul(MULTIPLY).wrapping_add(INCREMENT);
+        rand
+    }
+}
+
+
+fn shuffle<T>(elements: &mut [T]) {
+    let end = elements.len();
+    let mut rand = gen_rand(1024);
+    for i in 1..end {
+        for j in (1..i).rev() {
+            let p = rand() % (j + 1);
+            elements.swap(j, p);
+        }
+    }
+}
+
+fn qsort_by<T, F>(elements: &mut [T], cmp: &F)
+where F: Fn(&T, &T) -> std::cmp::Ordering {
+    if elements.len() < 10 {
+        insertion_sort_by(elements, cmp);
+        return;
+    }
+
+    let p = partition_by(elements, cmp);
+
+    qsort_by(&mut elements[..p], cmp);
+    qsort_by(&mut elements[p+1..], cmp);
+}
+
+fn partition_by<T, F>(elements: &mut [T], cmp: F) -> usize
+where F: Fn(&T, &T) -> std::cmp::Ordering {
+    let pivot = 0;
+    let end = elements.len();
+    let mut i = 1;
+    let mut j = end - 1;
+    loop {
+        while cmp(&elements[i], &elements[pivot]) == Ordering::Less {
+            if i == end - 1 { break; }
+            i += 1;
+        }
+        while cmp(&elements[pivot], &elements[j]) == Ordering::Less {
+            j -= 1;
+        }
+
+        if i >= j { break; }
+        elements.swap(i, j);
+    }
+    elements.swap(j, pivot);
+    j
+}
+
+fn insertion_sort_by<T, F>(elements: &mut [T], cmp: F)
+where F: Fn(&T, &T) -> std::cmp::Ordering {
+    let end = elements.len();
+    for i in 1..end {
+        for j in (1..=i).rev() {
+            if cmp(&elements[j], &elements[j-1]) != Ordering::Less {
+                break;
+            }
+            elements.swap(j, j-1);
+        }
+    }
+}
