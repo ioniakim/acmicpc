6ef6ca5f318043d43ce66e03b4d99665471a3008
S W Kim <ionia.kim@gmail.com>
2024-05-11 18:40:10 +0900

    Add 1764 to stage 14

---
+++ stage014/1764.rs
@@ -0,0 +1,21 @@
+use std::collections::BTreeSet;
+use std::io::{stdin, read_to_string, BufReader};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let input = read_to_string(BufReader::new(stdin()))?;
+
+    let mut iter = input.split_whitespace();
+    let mut next = || iter.next().ok_or::<String>("End of input".into());
+    let n = next()?.parse::<usize>()?;
+    let m = next()?.parse::<usize>()?;
+
+    let not_heard = (0..n).map(|_| next()).flatten().collect::<BTreeSet<&str>>();
+    let not_seen = (0..m).map(|_| next()).flatten().collect::<BTreeSet<&str>>();
+
+    let both = not_heard.intersection(&not_seen).map(|s| *s).collect::<Vec<_>>();
+
+    println!("{}", both.len());
+    println!("{}", both.join("\n"));
+
+    Ok(())
+}
