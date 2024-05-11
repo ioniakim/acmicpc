071a22f62f1951ce8df23f6807f5d9d8c6030952
S W Kim <ionia.kim@gmail.com>
2024-05-11 14:19:31 +0900

    Use BufReader to improve reading performance in 1620

--- stage014/1620.rs
+++ stage014/1620.rs
@@ -3,12 +3,13 @@ use std::io::{
     stdout,
     read_to_string,
     BufWriter,
-    Write
+    Write,
+    BufReader,
 };
 use std::collections::HashMap;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let input = read_to_string(stdin())?;
+    let input = read_to_string(BufReader::new(stdin()))?;
     let mut iter = input.split_ascii_whitespace();
     let mut next = || iter.next().ok_or::<String>("No More Input".into());
 
@@ -31,3 +32,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     Ok(())
 }
+
+
+// todo study the below code to improve performance
+
+// use std::{io, io::prelude::*};
+// use std::collections::HashMap;
+
+// fn main() {
+//     let mut stdin
+// 	= io::stdin().lock();
+//     let mut stdout
+// 	= io::stdout().lock();
+
+//     let mut buffer = String::with_capacity(1024 * 1024 * 40);
+//     let mut output_buffer = Vec::new();
+
+//     stdin.read_to_string(&mut buffer).expect("read failed");
+//     let mut lines
+// 	= buffer.lines().map(str::trim);
+
+//     let mut first
+// 	= lines.next().unwrap().split_whitespace();
+
+//     let n : usize = first.next().unwrap().parse().unwrap();
+//     let m : usize = first.next().unwrap().parse().unwrap();
+
+
+//     let mut vec = Vec::new();
+//     let mut map = HashMap::new();
+
+//     for (i,line) in (&mut lines).take(n).enumerate() {
+// 	vec.push(line);
+// 	map.insert(line, i + 1);
+//     }
+
+//     lines.take(m)
+// 	.for_each(|line| {
+// 	    let _ = match line.parse::<usize>() {
+// 		Ok(num) => writeln!(output_buffer, "{}", vec[num - 1]),
+// 		Err(_) => writeln!(output_buffer, "{}", map[line]),
+// 	    };
+// 	});
+
+//     let _ = io::copy(&mut output_buffer.as_slice(), &mut stdout);
+// }
